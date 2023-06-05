1b508b234200604ce92d8280bfabf3e93a43c3c7
Justin Dickenson <Justin@JJDsource.com>
2023-06-04 20:29:27 -0400

    add ssh setup and logout

--- linuxsetups/setupmachine.sh
+++ linuxsetups/setupmachine.sh
@@ -35,4 +35,27 @@ echo "$username:$password" | chpasswd
 # Add user to sudoers group
 usermod -aG sudo $username
 
+# Set up SSH with public key
+home_dir="/home/$username"
+ssh_dir="$home_dir/.ssh"
+
+# Create .ssh directory
+mkdir -p $ssh_dir
+chown $username:$username $ssh_dir
+chmod 700 $ssh_dir
+
+# Prompt for public key
+read -p "Enter the public key for SSH access: " public_key
+
+# Add public key to authorized_keys file
+echo $public_key >> $ssh_dir/authorized_keys
+chown $username:$username $ssh_dir/authorized_keys
+chmod 600 $ssh_dir/authorized_keys
+
 echo "User $username has been created and added to the sudoers group."
+echo "SSH access with the provided public key has been configured."
+
+# Logout of root account after 10 seconds
+echo "Logging out of the root account in 10 seconds..."
+sleep 10
+exit
